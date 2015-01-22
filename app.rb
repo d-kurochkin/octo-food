13fad08b6e722da7740838d2676532eafad791aa
Dmitriy Kurochkin <pengovan@gmail.com>
2015-01-22 17:41:29 +0600

    Just commit

--- app.rb
+++ app.rb
@@ -8,16 +8,17 @@ require './models'
 register Sinatra::Reloader
 set :bind, '0.0.0.0'
 
+
+
+
+
 get '/' do
   erb :index
 end
 
-get '/articles' do
-  # content_type :json
-
-  # @articles = Article.where(:parent => 'M').limit(12).map{|item| item.values}
-  # @articles.to_json
-  erb :articles, :layout => false, :locals => {:page => 10, :total => 50}
+get '/articles/*.*' do |category, page|
+  @articles = ArticleModel.get_page category, page.to_i
+  erb :articles, :layout => false, :locals => {page: page, articles:@articles}
 end
 
 
