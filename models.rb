f25c4f51fc314ab95fb883708030d41e841b123f
Dmitriy Kurochkin <pengovan@gmail.com>
2015-01-22 17:40:28 +0600

    Класс для получения записей

--- models.rb
+++ models.rb
@@ -9,7 +9,7 @@ DB.create_table :articles do
   String :code
   String :name
   String :parent
-  Boolean :is_group
+  Fixnum :is_group
   Float :price
 end
 
@@ -17,7 +17,13 @@ class Article < Sequel::Model;
 end
 
 # Populate the table
-ArticlesImport.get.each { |article| Article.insert(article)}
+ArticlesImport.get.each { |article| Article.insert(article) }
 
 # Print out the average price
 puts "Импортировано записей: #{Article.count}"
+
+class ArticleModel
+  def self.get_page category, page
+    Article.where(:parent => category).limit(12, page*12).map { |item| item.values }
+  end
+end
