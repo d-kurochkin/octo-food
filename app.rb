b9c891a1e02dee263b94d56b59d8408cb6f2e87a
Dmitriy Kurochkin <pengovan@gmail.com>
2015-01-22 16:36:59 +0600

    Обработка получения артикулов

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
+require 'json'
+
+require './models'
 
 register Sinatra::Reloader
 set :bind, '0.0.0.0'
@@ -8,4 +11,13 @@ set :bind, '0.0.0.0'
 get '/' do
   erb :index
 end
+
+get '/articles' do
+  # content_type :json
+
+  # @articles = Article.where(:parent => 'M').limit(12).map{|item| item.values}
+  # @articles.to_json
+  erb :articles, :layout => false, :locals => {:page => 10, :total => 50}
+end
+
 
