239d6e2fdae7cc2e10c4c088b60ee127e7dc6047
Dmitriy Kurochkin <pengovan@gmail.com>
2015-01-22 15:10:59 +0600

    Реализован импорт всех записей из xml

--- articles_import.rb
+++ articles_import.rb
@@ -11,18 +11,22 @@ MAPPINGS = {
 IMPORT_PATH = 'menu.ekz'
 
 class ArticlesImport
-  def self.load
-    file = File.new IMPORT_PATH
-    content = file.read
-
-    parser = Nori.new
-    result = parser.parse(content)
-    result['N']['NOM']
+  def self.get
+    items = ArticlesImport.load
+    ArticlesImport.transform items
   end
 
-  def self.transform
-    articles = items.map { |item| Hash[item.map {|k, v| [MAPPINGS[k], v] }]}
-    articles.map do |item|
+  private
+  def self.load
+    file = File.new IMPORT_PATH
+    parser = Nori.new
+
+    parser.parse(file.read)['N']['NOM']
+  end
+
+  def self.transform items
+    items.map! { |item| Hash[item.map {|k, v| [MAPPINGS[k], v] }]}
+    items.map do |item|
       item.tap do |i|
         i['is_group'] =i['is_group'].to_i
         i['price'] =i['price'].to_f
