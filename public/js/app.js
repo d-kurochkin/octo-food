235f86fdd9c57d86ac007e30eb3181aa6372ec08
Dmitriy Kurochkin <newpengo@mail.ru>
2015-03-12 21:59:01 +0600

    Реформат файлов

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,23 @@
-
-
-
 function loadPage(category, page) {
-    $("#articles-content").load('/articles/' + category + '.' + page);
+  $("#articles-content").load('/articles/' + category + '.' + page);
 }
 
 function sendOrder() {
-    var order = {};
-    order.charge = $('#charge').val();
+  var order = {};
+  order.charge = $('#charge').val();
 
-    $.post('/order', order, function (data) {
-        if (data == '0') {
-            emptyOrder();
-            $('#paymentModal').modal('hide');
+  $.post('/order', order, function (data) {
+    if (data == '0') {
+      emptyOrder();
+      $('#paymentModal').modal('hide');
 
-            loadPage("M", 0);
-        }
-    });
+      loadPage("M", 0);
+    }
+  });
 }
 
 $(document).ready(function () {
-    //$.get('/display/hello');
-    $("#articles-content").load('/articles/M.0');
+  //$.get('/display/hello');
+  $("#articles-content").load('/articles/M.0');
 });
 
