b49fa8b36d6310d1cd9c8d1484955063cbe0217c
Dmitriy Kurochkin <newpengo@mail.ru>
2015-03-12 21:25:00 +0600

    Функциональность OrderList вынесена в отдельный файл

---
+++ public/js/order_list.js
@@ -0,0 +1,56 @@
+function emptyOrderList() {
+    localStorage.setItem('order_list', "{}");
+    updateOrderListState({});
+}
+
+function getOrderList() {
+    return JSON.parse(localStorage.getItem('order_list')) || {};
+}
+
+function setOrderList(order_list) {
+    localStorage.setItem('order_list', JSON.stringify(order_list));
+    updateOrderListState(order_list);
+}
+
+function updateOrderListState(order_list){
+    mountedOrderPanel.setState({order_list: order_list});
+}
+
+function getOrderListTotal(order_list) {
+    var sum_items = _.mapValues(order_list, function (item) {
+        return item.price * item.count;
+    });
+
+    return _.sum(sum_items);
+}
+
+function addArticle(name, code, price) {
+    var order_list = getOrderList();
+
+    if (order_list[code]) {
+        order_list[code].count += 1;
+    } else {
+        order_list[code] = {
+            name: name,
+            code: code,
+            price: price,
+            count: 1
+        };
+    }
+
+    setOrderList(order_list);
+}
+
+function removeArticle(code) {
+    var order_list = getOrderList();
+
+    if (order_list[code] && order_list[code].count > 1) {
+        order_list[code].count -= 1;
+    } else {
+        order_list = _.omit(order_list, code);
+    }
+
+    setOrderList(order_list);
+}
+
+
